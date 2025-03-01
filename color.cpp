1a2efb586f60b9f118c22388bf968b9533abf99b
blackmirroxx <olivier.mutschler@gmail.com>
2025-03-01 13:44:54 +0100

    color in C++17

--- color.cpp
+++ color.cpp
@@ -23,15 +23,11 @@ auto print_in_middle(WINDOW *win, int starty, int startx, int width, const char 
 {	int length, x, y;
 	float temp;
 
-	if(win == NULL)
-		win = stdscr;
+	if(win == NULL) win = stdscr;
 	getyx(win, y, x);
-	if(startx != 0)
-		x = startx;
-	if(starty != 0)
-		y = starty;
-	if(width == 0)
-		width = 80;
+	if(startx != 0) x = startx;
+	if(starty != 0) y = starty;
+	if(width == 0) width = 80;
 
 	length = strlen(string);
 	temp = (width - length)/ 2;
