58d9679d05e5154e5083cde0667097e34ccb25ac
Olivier Mutschler -- ricardo <olivier.mutschler@gmail.com>
2025-08-08 15:31:47 +0200

    Derive -> enum

--- box4-rs/src/main.rs
+++ box4-rs/src/main.rs
@@ -1,3 +1,17 @@
+use ncurses::*; 
+use std::env;
+
+#[derive(Debug)]
+enum Colors {
+    White, 
+    Yellow, 
+    Red, 
+    Blue, 
+    Green, 
+    Black
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
