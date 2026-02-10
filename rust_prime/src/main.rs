7e880230e466758ebcad86ee76d685439655f46a
Olivier Mutschler -- ricardo <olivier.mutschler@gmail.com>
2026-02-10 18:23:08 +0100

    rewritten m17.cpp -> rust_prime

--- rust_prime/src/main.rs
+++ rust_prime/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
+// trouve le plus petit nombre premier strictement supérieur à `b`.
+fn least_prime_greater_than(b: i32) -> Option<i32> {
+    let primes = vec![2, 3, 5, 7, 11, 13, 19, 23, 29];
+    primes.into_iter().find(|&v| v > b)
+}
+
 fn main() {
     println!("Hello, world!");
 }
