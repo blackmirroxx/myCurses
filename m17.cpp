8885a6388253d5d4df6cb11e95e4666779641f31
Olivier Mutschler -- ricardo <olivier.mutschler@gmail.com>
2026-02-10 17:05:26 +0100

    rustling -> I did it XD

--- m17.cpp
+++ m17.cpp
@@ -8,9 +8,8 @@
 // prototype 
 int leastPrimeGreaterThan(int);
 
-// using namespace std;
-
-int main(void){
+auto main(void) -> int 
+{
   auto test = 1.0/4.0; // int or float ? 
   int p = 0;
   std::cout << test << std::endl;
@@ -21,10 +20,11 @@ int main(void){
 }
 
 // lambda prime
-int leastPrimeGreaterThan(int b){
+auto leastPrimeGreaterThan(int b) -> int 
+{
    std::vector <int> primes {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-   
    return *std::find_if(primes.begin(), primes.end(), [b] (int v) {return v > b; }); 
 }
 
-// compile it with g++ -std=c++17
+// compile it with g++ -std=c++17 -o prime m17 
+// execute ./prime 
